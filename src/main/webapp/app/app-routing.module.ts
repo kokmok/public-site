5664b02f8413d6e2b3d73f080d535907511c7d7c
Jona <jona303@gmail.com>
2022-09-06 09:37:52 +0200

    add page title and description

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import {Injectable, NgModule} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterModule} from '@angular/router';
-import {errorRoute} from './layouts/error/error.route';
-import {navbarRoute} from './layouts/navbar/navbar.route';
-import {DEBUG_INFO_ENABLED} from 'app/app.constants';
-import {CoursComponent} from 'app/cours/cours.component';
-import {CoursDetailComponent} from 'app/cours/cours-detail/cours-detail.component';
-import {Ue} from 'app/shared/model/ue.model';
-import {UeService} from 'app/shared/services/ue.service';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {TechnologiesComponent} from 'app/technologies/technologies.component';
+import { Injectable, NgModule } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterModule } from '@angular/router';
+import { errorRoute } from './layouts/error/error.route';
+import { navbarRoute } from './layouts/navbar/navbar.route';
+import { DEBUG_INFO_ENABLED } from 'app/app.constants';
+import { CoursComponent } from 'app/cours/cours.component';
+import { CoursDetailComponent } from 'app/cours/cours-detail/cours-detail.component';
+import { Ue } from 'app/shared/model/ue.model';
+import { UeService } from 'app/shared/services/ue.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TechnologiesComponent } from 'app/technologies/technologies.component';
 
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
 
@@ -17,7 +17,7 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
 export class UeResolver implements Resolve<Ue> {
   constructor(private service: UeService) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<any>|Promise<any>|any {
+  resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> | any {
     return this.service.findBySlug(route.paramMap.get('slug') || '').pipe(map(res => res.body));
   }
 }
@@ -30,16 +30,22 @@ export class UeResolver implements Resolve<Ue> {
           path: 'unites-d-enseignement/:slug',
           component: CoursDetailComponent,
           resolve: {
-            ue: UeResolver
-          }
+            ue: UeResolver,
+          },
         },
         {
           path: 'unites-d-enseignement',
-          component: CoursComponent
+          component: CoursComponent,
+          data: {
+            pageTitle: 'besWebdevApp.ue.pageTitle',
+          },
         },
         {
           path: 'technologies',
-          component: TechnologiesComponent
+          component: TechnologiesComponent,
+          data: {
+            pageTitle: 'global.technologies.pageTitle',
+          },
         },
         ...LAYOUT_ROUTES,
       ],
